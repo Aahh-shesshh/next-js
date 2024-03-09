d261d78145cfd6ec659fef9b890b11a1a9b6f0ee
Ashish Khatri <asiskhatri123@gmail.com>
2024-03-09 21:11:03 +0545

    (feat): added cv in the website

--- src/layout/base-layout.jsx
+++ src/layout/base-layout.jsx
@@ -4,14 +4,7 @@ import { Provider } from "react-redux";
 import Footer from "@/Main/footer";
 
 export default function BaseLayout({ children }) {
-  // const [showLoading, setShowLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowLoading(false);
-  //   }, 2000);
-  //   return () => clearInterval(timer);
-  // }, []);
 
   return (
     <Provider store={store}>
